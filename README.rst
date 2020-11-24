4d421c471e2886686d275698353beef8dc4d7b5e
Fernando Espíndola <fer.esp@gmail.com>
2020-11-24 10:21:26 -0300

    Fixing the form we present on the page

--- README.rst
+++ README.rst
@@ -2,8 +2,15 @@
 Colaborativa
 ============
 
+
 Versions
 ========
 
 - Python 3.5.2
 - Django 2.2.15
+
+
+References
+==========
+
+https://edx.readthedocs.io/projects/edx-installing-configuring-and-running/en/latest/configuration/customize_registration_page.html
--- colaborativa/forms.py
+++ colaborativa/forms.py
@@ -7,13 +7,23 @@ class ExtraInfoForm(ModelForm):
     The fields on this form are derived from the ExtraInfo model in models.py.
     """
 
-    def __init__(self, *args, **kwargs):
-        super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields["favorite_movie"].error_messages = {
-            "required": u"Please tell us your favorite movie.",
-            "invalid": u"We're pretty sure you made that movie up.",
-        }
+    # def __init__(self, *args, **kwargs):
+    #     super(ExtraInfoForm, self).__init__(*args, **kwargs)
+    #     self.fields["favorite_movie"].error_messages = {
+    #         "required": u"Please tell us your favorite movie.",
+    #         "invalid": u"We're pretty sure you made that movie up.",
+    #     }
 
     class Meta(object):
         model = ExtraInfo
-        fields = ("favorite_editor", "favorite_movie")
+        fields = (
+            "cpf",
+            "gender",
+            "profession",
+            "occupation_area",
+            "hospital_work",
+            "city",
+            "state",
+            "health_hands",
+            "health_hands_where",
+        )
